79d938729e78ec96941d0810271e27c339fd23e9
ThorbenJensen <jensen.thorben@gmail.com>
2017-09-21 22:52:58 +0200

    clean lintr

--- src/prizing_from_observations.R
+++ src/prizing_from_observations.R
@@ -1,9 +1,9 @@
 ### estimate values of good, expressed relative to the least valuable one
 
 get_exchange_rates <- function(transactions) {
-  
-  fit <-lm(c ~ a + b + 0, data = transactions)
-  
+
+  fit <- lm(c ~ a + b + 0, data = transactions)
+
   # return estimated values of other items
   result <- fit$coefficients * -1
   return(data.frame(result))
